86d83be383e811c3de0f520f770d5220c02741d8
Swagata Roy <swagataroy534@gmail.com>
2022-11-28 11:12:17 +0530

    major-updates

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,11 @@
-import React from "react";
-import millify from "millify";
-import { Typography, Row, Col, Statistic } from "antd";
-import { Link } from "react-router-dom";
+import React from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
 
-import { useGetCryptosQuery } from "../services/cryptoApi";
-import { Cryptocurrencies, News } from "../components";
-import Loader from "./Loader";
+import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components';
+import Loader from './Loader';
 
 const { Title } = Typography;
 
@@ -19,53 +19,57 @@ const Homepage = () => {
 
   return (
     <>
-      <Title level={2} className="heading">
+      <Title level={3} className='heading'>
         Global Crypto Stats
       </Title>
       <Row>
-        <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
+        <Col lg={4} md={6} xs={12}>
+          <Statistic title='Total Cryptocurrencies' value={globalStats.total} valueStyle={{ fontSize: '18px' }} />
         </Col>
-        <Col span={12}>
+        <Col lg={4} md={6} xs={12}>
           <Statistic
-            title="Total Exchanges"
+            title='Total Exchanges'
             value={millify(globalStats.totalExchanges)}
+            valueStyle={{ fontSize: '18px' }}
           />
         </Col>
-        <Col span={12}>
+        <Col lg={4} md={6} xs={12}>
           <Statistic
-            title="Total Market Cap"
+            title='Total Market Cap'
             value={millify(globalStats.totalMarketCap)}
+            valueStyle={{ fontSize: '18px' }}
           />
         </Col>
-        <Col span={12}>
+        <Col lg={4} md={6} xs={12}>
           <Statistic
-            title="Total 24h Volume"
+            title='Total 24h Volume'
             value={millify(globalStats.total24hVolume)}
+            valueStyle={{ fontSize: '18px' }}
           />
         </Col>
-        <Col span={12}>
+        <Col lg={4} md={6} xs={12}>
           <Statistic
-            title="Total Markets"
+            title='Total Markets'
             value={millify(globalStats.totalMarkets)}
+            valueStyle={{ fontSize: '18px' }}
           />
         </Col>
       </Row>
-      <div className="home-heading-container">
-        <Title level={2} className="home-title">
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>
           Top 10 Cryptocurrencies in the world
         </Title>
-        <Title level={3} className="show-more">
-          <Link to="/cryptocurrencies">Show More</Link>
+        <Title level={3} className='show-more'>
+          <Link to='/cryptocurrencies'>Show More</Link>
         </Title>
       </div>
       <Cryptocurrencies simplified />
-      <div className="home-heading-container">
-        <Title level={2} className="home-title">
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>
           Latest Crypto News
         </Title>
-        <Title level={3} className="show-more">
-          <Link to="/news">Show More</Link>
+        <Title level={3} className='show-more'>
+          <Link to='/news'>Show More</Link>
         </Title>
       </div>
       <News simplified />
